32f44133d422aca2365db812c1035a6149080c4d
h8m0ss <danarmenta32@gmail.com>
2021-12-31 15:21:31 -0600

    updated class to work with new deletion API

--- lib/app/messages/message_list_view.dart
+++ lib/app/messages/message_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/common/models/chat_message.dart';
+import 'package:flutter_chat_app/common/models/message_widget_style.dart';
 import 'package:flutter_chat_app/common/services/auth_service.dart';
 import 'package:flutter_chat_app/common/services/firestore_server.dart';
 import 'package:provider/provider.dart';
@@ -7,9 +8,11 @@ import 'package:provider/provider.dart';
 import 'message_widget.dart';
 
 class MessageListView extends StatefulWidget {
-  const MessageListView({Key? key, this.scrollController}) : super(key: key);
+  const MessageListView({Key? key, this.scrollController, this.onDelete})
+      : super(key: key);
 
   final ScrollController? scrollController;
+  final Future<void> Function(ChatMessage)? onDelete;
 
   @override
   State<MessageListView> createState() => _MessageListViewState();
@@ -27,15 +30,33 @@ class _MessageListViewState extends State<MessageListView> {
           }
           return Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListView.builder(
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(height: 16),
               reverse: true,
               controller: widget.scrollController,
               itemBuilder: (context, index) {
-                return MessageWidget(
-                  message: snapshot.data![index],
-                  currentUser: Provider.of<AuthService>(context).currentUser!,
-                  onDelete: () => print('delete ${snapshot.data![index].text}'),
-                  onReport: () => print('report ${snapshot.data![index].text}'),
+                bool isCurrentUser =
+                    Provider.of<AuthService>(context).currentUser! ==
+                        snapshot.data![index].sender;
+                return Row(
+                  children: [
+                    if (isCurrentUser) Flexible(child: Container(), flex: 6),
+                    Flexible(
+                      flex: 5,
+                      child: MessageWidget(
+                        message: snapshot.data![index],
+                        style: isCurrentUser
+                            ? MessageWidgetStyle.byUser()
+                            : MessageWidgetStyle.byOther(),
+                        buttonLabel: isCurrentUser && widget.onDelete != null
+                            ? 'Delete'
+                            : null,
+                        onButtonPressed: () =>
+                            widget.onDelete!(snapshot.data![index]),
+                      ),
+                    ),
+                    if (!isCurrentUser) Flexible(child: Container(), flex: 6),
+                  ],
                 );
               },
               itemCount: snapshot.data!.length,
