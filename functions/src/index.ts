7fc03427e8a9a232780c5d74703cadd3bf967d66
h8m0ss <danarmenta32@gmail.com>
2022-06-06 21:09:32 -0500

    added database mantainance function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 import Filter = require("bad-words");
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
 export const censorMessages = functions.firestore
   .document("/messages/{documentId}")
   .onCreate((snap, context) => {
@@ -23,4 +22,27 @@ export const censorMessages = functions.firestore
       return snap.ref.set({ text: result }, { merge: true });
     }
     return null;
+  });
+
+const MAX_MESSAGES = 200;
+
+export const removeOld = functions.pubsub
+  .schedule("0 0 1 * *")
+  .onRun(async (context) => {
+    const docs = await admin
+      .firestore()
+      .collection("/messages")
+      .orderBy("sent", "desc")
+      .get();
+
+    if (docs.docs.length > MAX_MESSAGES) {
+      functions.logger.log({
+        action: "Remove old",
+        initialLength: docs.docs.length,
+      });
+
+      for (let i = MAX_MESSAGES; i < docs.docs.length; i++) {
+        await admin.firestore().doc(`/messages/${docs.docs[i].id}`).delete();
+      }
+    }
   });
